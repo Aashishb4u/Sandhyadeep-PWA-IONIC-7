c1de71785d8cedcbf253a0f4339bd4fcafafb04c
Aashish Bhagwat <aashishbhagwat@Aashishs-MBP.domain.name>
2023-12-26 22:53:20 +0530

    Added PWA and dependancies resolved

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import {enableProdMode, importProvidersFrom} from '@angular/core';
+import {enableProdMode, importProvidersFrom, isDevMode} from '@angular/core';
 import {bootstrapApplication, BrowserModule} from '@angular/platform-browser';
 import {
     RouteReuseStrategy,
@@ -15,6 +15,7 @@ import {ApiService} from "./app/shared-services/api.service";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {HttpTokenInterceptorsService} from "./app/shared-services/authentication/http-token-interceptors.service";
+import { provideServiceWorker } from '@angular/service-worker';
 
 if (environment.production) {
     enableProdMode();
@@ -22,12 +23,16 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
     providers: [
-        ApiService,
-        importProvidersFrom([HttpClientModule, BrowserAnimationsModule, BrowserModule], BrowserAnimationsModule),
-        {provide: HTTP_INTERCEPTORS, useClass: HttpTokenInterceptorsService, multi: true},
-        {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
-        importProvidersFrom(IonicModule.forRoot({})),
-        // Important: We are using PreloadModules to load all the modules after first load
-        provideRouter(routes, withPreloading(PreloadAllModules)),
-    ],
+    ApiService,
+    importProvidersFrom([HttpClientModule, BrowserAnimationsModule, BrowserModule], BrowserAnimationsModule),
+    { provide: HTTP_INTERCEPTORS, useClass: HttpTokenInterceptorsService, multi: true },
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    importProvidersFrom(IonicModule.forRoot({})),
+    // Important: We are using PreloadModules to load all the modules after first load
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+],
 });
